3787b895a6337229fd220c1237d9a6af600cb922
Tobias Schubotz <tobias.schubotz@gmail.com>
2019-02-15 19:23:33 +0100

    Fix package

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description='Python code to generate Ethereum addresses from a hierarchical deterministic wallet according to the BIP44 standard.',
     author='Michail Brynard',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'rlp',
         'eth-utils',
